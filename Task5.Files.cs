ae6e289d50b246d847edf7c25d9e15a2657a449b
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2020-02-04 22:36:15 +0200

    Add files via upload

---
+++ Task5.Files.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Animal animal = new Animal("Tara", 5, 1.70);
+            Stream stream = File.Open("D:/Animal.txt", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, animal);
+            stream.Close();
+            animal = null;
+            stream = File.Open("AnimalData.dat", FileMode.Open);
+            bf = new BinaryFormatter();
+            animal = (Animal)bf.Deserialize(stream);
+            stream.Close();
+            Console.WriteLine(animal);
+        }
+    }
+}
